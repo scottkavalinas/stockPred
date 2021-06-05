8d8d6208cd0e1f6e9504c79b2bb90236202fb3ed
Scott Kavalinas <36358126+scottkavalinas@users.noreply.github.com>
2021-06-04 19:56:19 -0600

    project started

---
+++ stock.py
@@ -0,0 +1,20 @@
+import yfinance
+import numpy as np
+import random
+
+price = 10.0
+earning = 100000.4
+expenses = 100000.5
+ratio_1 = 3.0
+ratio_2 = 5.0
+ratio_3 = 2.0
+
+data = np.array([price,earning,expenses,ratio_1,ratio_2,ratio_3])
+
+for i in range(100):
+	num = random.randint
+	x = np.array(num)
+	x = data *x
+	print(x)
+
+price_2 = np.array([9.0])
