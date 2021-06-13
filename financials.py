1659ee49b63834b81a2ec25224f32b0093dfe367
ScottKavalinas <36358126+scottkavalinas@users.noreply.github.com>
2021-06-12 20:23:02 -0600

    begin importing data

--- financials.py
+++ financials.py
@@ -1,5 +1,27 @@
 import yfinance
+from yfinance import base
+from yfinance import Ticker,multi
+from yfinance import Tickers
+from yfinance import ticker
+from yfinance import tickers
 
+finList=['MSFT','AAPL']
 
+quarterlies = []
 
-finList=['MSFT','AAPL','AC']
+for i in finList:
+	break
+	stock = base.TickerBase(i)
+	print(stock.get_earnings(freq='quarterly'))
+
+for i in finList:
+	stock = base.TickerBase(i)
+	print(stock.get_balance_sheet(freq='quarterly'))
+
+for i in finList:
+	break
+	stock = Ticker(i)
+	print(stock.history(period="1mo", interval="1d",
+                start=None, end=None, prepost=False,
+                actions=True, auto_adjust=True, proxy=None,
+                threads=True, group_by='column', progress=True))
